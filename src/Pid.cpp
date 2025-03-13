182d0f4186ea1236d2751b7ef92d3444bc79a62b
ygdstmidn <2022d12@stu.gifu-nct.ac.jp>
2025-03-13 11:42:48 +0900

    🎨 namespace Pidはいらない 名前空間Pidの中にはclass Pidただ一つだったので． Pid::Pid instance;はばからしい

--- src/Pid.cpp
+++ src/Pid.cpp
@@ -2,45 +2,42 @@
 
 namespace ygdstmidn
 {
-    namespace Pid
+
+    Pid::Pid(PidBase::PidParam param)
     {
+        param_ = param;
+        reset();
+    }
 
-        Pid::Pid(PidBase::PidParam param)
+    Pid::~Pid()
+    {
+    }
+
+    void Pid::reset()
+    {
+        integral_ = 0;
+        preError_ = NAN;
+    }
+
+    float Pid::calc(float target, float current, float dt)
+    {
+        return calc(target - current, dt);
+    }
+
+    float Pid::calc(float error, float dt)
+    {
+        integral_ += error * dt;
+        float derivative = (error - preError_) / dt;
+        if (std::isnan(preError_))
         {
-            param_ = param;
-            reset();
+            derivative = 0;
         }
 
-        Pid::~Pid()
-        {
-        }
+        preError_ = error;
 
-        void Pid::reset()
-        {
-            integral_ = 0;
-            preError_ = NAN;
-        }
+        float output = param_.gain.kp * error + param_.gain.ki * integral_ + param_.gain.kd * derivative;
+        output = PidBase::clamp(output, param_.min, param_.max);
+        return output;
+    }
 
-        float Pid::calc(float target, float current, float dt)
-        {
-            return calc(target - current, dt);
-        }
-
-        float Pid::calc(float error, float dt)
-        {
-            integral_ += error * dt;
-            float derivative = (error - preError_) / dt;
-            if (std::isnan(preError_))
-            {
-                derivative = 0;
-            }
-
-            preError_ = error;
-
-            float output = param_.gain.kp * error + param_.gain.ki * integral_ + param_.gain.kd * derivative;
-            output = PidBase::clamp(output, param_.min, param_.max);
-            return output;
-        }
-
-    } // namespace Pid
 } // namespace ygdstmidn
